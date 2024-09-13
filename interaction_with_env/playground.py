717fbeeda4536cb88b0f7f3873ed4f66d63f7f8c
Ni Dang <47034704+Dawn0916@users.noreply.github.com>
2024-09-13 16:47:23 +0200

    Update playground.py

--- interaction_with_env/playground.py
+++ interaction_with_env/playground.py
@@ -1,12 +1,11 @@
 import gymnasium as gym
 import numpy as np
+from matplotlib import pyplot as plt
 from tqdm import tqdm
-
-env = gym.make("CartPole-v1", render_mode = "rgb_array")
-
-total_reward= 0
+# Create an environment
+env = gym.make("CartPole-v1", render_mode="rgb_array")
+total_reward = 0
 nr_episodes = 1000
-
 nr_actions = env.action_space.n
 for _ in tqdm(range(nr_episodes)):
     env.reset()
@@ -14,9 +13,13 @@ for _ in tqdm(range(nr_episodes)):
     truncated = False
     while not (terminated or truncated):
         chosen_action = np.random.randint(nr_actions)
-        s_prime, r, terminated, truncated, info = env.step(action = chosen_action)
+        s_prime, r, terminated, truncated, info = env.step(action=chosen_action)
         total_reward += r
-
+        state_in_rgb = env.render()
+        # plot the state which is a 3D array with rgb values
+        plt.imshow(state_in_rgb)
+        # wait for 0.1 seconds
+        plt.pause(0.1)
 print(total_reward / nr_episodes)
 """ import gymnasium as gym
 # Create an environment
